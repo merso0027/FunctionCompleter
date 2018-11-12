70c1047fa7c54593d0e483192247e28e1302253b
dgalas <dgalas@execom.eu>
2018-11-12 15:28:20 +0100

    Remove tests

--- AutoCompleteTextBox/Window1.xaml.cs
+++ AutoCompleteTextBox/Window1.xaml.cs
@@ -15,9 +15,6 @@ using System.ComponentModel;
 
 namespace AutoCompleteTextBox
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
